bc0aec33bb539220f6b099197cc383ab1adf83fe
Akash Mondal <akmo3901@gmail.com>
2021-02-13 17:47:00 +0000

    Added Code for LNMIIT Contest

---
+++ Ide.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int r;
+	cin >> r;
+	if(r >= 768800) {
+	    cout << "aham brahmasmi";
+	} else {
+	    cout << "-1";
+	}
+	return 0;
+}
